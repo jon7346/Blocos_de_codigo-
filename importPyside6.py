930d3415302cb721dd5bd014dd003bb8cda9637a
Jonathan <Jonathanfelipe7346@gmail.com>
2025-10-28 12:40:10 -0300

    importações do pyside6

---
+++ importPyside6.py
@@ -0,0 +1,7 @@
+import sys
+from PySide6.QtWidgets import (
+    QApplication, QWidget, QVBoxLayout, QHBoxLayout, 
+    QLabel, QLineEdit, QPushButton, QFrame
+)
+from PySide6.QtGui import QFont # Para estilizar a fonte
+from PySide6.QtCore import Qt  # Para alinhamento
